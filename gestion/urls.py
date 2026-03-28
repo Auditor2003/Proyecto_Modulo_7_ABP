dab95f4d2defc62d462fc7a560dcd887b8c32f9e
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-28 11:01:51 -0300

    Agrego ruta para crear usuarios y conecto CreateView en urls

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UsuarioListView
+from .views import UsuarioListView, UsuarioCreateView
 
 urlpatterns = [
+    # Aquí dejo la ruta para listar usuarios
     path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
+
+    # Aquí agrego la ruta para crear un nuevo usuario
+    path('usuarios/crear/', UsuarioCreateView.as_view(), name='usuario_create'),
 ]
