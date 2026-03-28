605b347b7f4021ecf56615f59fc49158e2a516a5
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-28 11:34:47 -0300

    Agrego ruta para editar usuarios usando pk y conecto UpdateView

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import UsuarioListView, UsuarioCreateView
+from .views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView
 
 urlpatterns = [
-    # Aquí dejo la ruta para listar usuarios
+    # Aquí dejo la lista de usuarios
     path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
 
-    # Aquí agrego la ruta para crear un nuevo usuario
+    # Aquí dejo la creación de usuarios
     path('usuarios/crear/', UsuarioCreateView.as_view(), name='usuario_create'),
+
+    # Aquí agrego la edición de usuario (uso el id del usuario)
+    path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
+]
 ]
