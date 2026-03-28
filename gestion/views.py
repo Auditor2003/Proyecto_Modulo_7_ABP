1b42816bec3d4d2ff5a6429cc7fad130856d8680
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-28 11:33:08 -0300

    Agrego vista para editar usuarios reutilizando el formulario existente

--- gestion/views.py
+++ gestion/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Usuario
 from .forms import UsuarioForm
@@ -18,5 +18,14 @@ class UsuarioCreateView(CreateView):
     form_class = UsuarioForm
     success_url = reverse_lazy('usuario_list')
 
-    # Aquí uso un formulario personalizado en lugar de fields
-    # Esto me permite agregar validaciones como confirmación de contraseña
+    # Aquí uso un formulario personalizado con validaciones
+
+
+class UsuarioUpdateView(UpdateView):
+    model = Usuario
+    template_name = 'gestion/usuario_form.html'
+    form_class = UsuarioForm
+    success_url = reverse_lazy('usuario_list')
+
+    # Aquí reutilizo el mismo formulario para editar usuarios
+    # Django carga automáticamente los datos actuales del usuario
