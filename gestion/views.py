78e9c5c7216b020a2de14d25805679ea316a2cd2
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-28 11:00:32 -0300

    Agrego vista para crear usuarios usando CreateView y preparo formulario automático

--- gestion/views.py
+++ gestion/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Usuario
 
 
@@ -6,3 +7,17 @@ class UsuarioListView(ListView):
     model = Usuario
     template_name = 'gestion/usuario_list.html'
     context_object_name = 'usuarios'
+
+    # Aquí estoy mostrando todos los usuarios en una lista simple
+    # Uso context_object_name para poder recorrerlos fácil en el template
+
+
+class UsuarioCreateView(CreateView):
+    model = Usuario
+    template_name = 'gestion/usuario_form.html'
+    fields = ['nombre', 'correo_electronico', 'contrasena', 'saldo']
+    success_url = reverse_lazy('usuario_list')
+
+    # Aquí estoy creando un formulario automático con los campos del modelo
+    # Cuando el usuario envía el formulario, se guarda en la BD
+    # Después redirijo a la lista de usuarios
