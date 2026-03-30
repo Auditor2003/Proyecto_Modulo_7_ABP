b1e4366db660aa8cc0d4504e6da867b66daa6a5e
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-30 17:16:36 -0300

    Verificar configuración de rutas principales del proyecto

--- gestion/urls.py
+++ gestion/urls.py
@@ -3,19 +3,17 @@ from .views import (
     UsuarioListView,
     UsuarioCreateView,
     UsuarioUpdateView,
-    UsuarioDeleteView
+    UsuarioDeleteView,
+    TransaccionCreateView
 )
 
 urlpatterns = [
-    # Lista de usuarios
-    path('usuarios/', UsuarioListView.as_view(), name='usuario_list'),
+    # Usuario
+    path('', UsuarioListView.as_view(), name='usuario_list'),
+    path('crear/', UsuarioCreateView.as_view(), name='usuario_create'),
+    path('editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
+    path('eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 
-    # Crear usuario
-    path('usuarios/crear/', UsuarioCreateView.as_view(), name='usuario_create'),
-
-    # Editar usuario
-    path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
-
-    # Eliminar usuario
-    path('usuarios/eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
+    # Transaccion
+    path('transaccion/crear/', TransaccionCreateView.as_view(), name='transaccion_create'),
 ]
