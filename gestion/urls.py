e2eb889c6832e4bdbb3d8c76c1166aeb2c23def1
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-30 21:10:28 -0300

    Corrige rutas usuario_update y usuario_delete en urls

--- gestion/urls.py
+++ gestion/urls.py
@@ -3,17 +3,20 @@ from .views import *
 
 urlpatterns = [
     path('', UsuarioListView.as_view(), name='usuario_list'),
-    path('crear/', UsuarioCreateView.as_view(), name='usuario_create'),
 
+    # CRUD Usuario completo
+    path('crear/', UsuarioCreateView.as_view(), name='usuario_create'),
+    path('editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
+    path('eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
+
+    # Moneda
     path('moneda/', MonedaListView.as_view(), name='moneda_list'),
     path('moneda/crear/', MonedaCreateView.as_view(), name='moneda_create'),
 
+    # Transacciones
     path('transaccion/', TransaccionListView.as_view(), name='transaccion_list'),
     path('transaccion/crear/', TransaccionCreateView.as_view(), name='transaccion_create'),
 
-    # Beneficiarios 
-    path('beneficiario/', BeneficiarioListView.as_view(), name='beneficiario_list'),
+    # Beneficiario
     path('beneficiario/crear/', BeneficiarioCreateView.as_view(), name='beneficiario_create'),
-    path('beneficiario/editar/<int:pk>/', BeneficiarioUpdateView.as_view(), name='beneficiario_update'),
-    path('beneficiario/eliminar/<int:pk>/', BeneficiarioDeleteView.as_view(), name='beneficiario_delete'),
 ]
