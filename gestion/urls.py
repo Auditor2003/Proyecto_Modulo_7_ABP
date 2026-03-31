fab8b860e1673014438b9eede70e4c2cfe8521e5
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-30 22:52:30 -0300

    Agrega rutas completas para CRUD de beneficiarios

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,24 +1,36 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
+
+    # HOME (usuarios)
     path('', UsuarioListView.as_view(), name='usuario_list'),
 
+    # USUARIO
     path('crear/', UsuarioCreateView.as_view(), name='usuario_create'),
     path('editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
     path('eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 
+    # MONEDA
     path('moneda/', MonedaListView.as_view(), name='moneda_list'),
     path('moneda/crear/', MonedaCreateView.as_view(), name='moneda_create'),
     path('moneda/editar/<int:pk>/', MonedaUpdateView.as_view(), name='moneda_update'),
     path('moneda/eliminar/<int:pk>/', MonedaDeleteView.as_view(), name='moneda_delete'),
 
+    # TRANSACCIONES
     path('transaccion/', TransaccionListView.as_view(), name='transaccion_list'),
     path('transaccion/crear/', TransaccionCreateView.as_view(), name='transaccion_create'),
 
-    path('beneficiario/crear/', BeneficiarioCreateView.as_view(), name='beneficiario_create'),
-
+    # DEPOSITO
     path('deposito/', DepositoCreateView.as_view(), name='deposito_create'),
 
+    # CARTOLA
     path('cartola/<int:usuario_id>/', CartolaUsuarioView.as_view(), name='cartola_usuario'),
+
+    # BENEFICIARIO (CRUD COMPLETO)
+    path('beneficiario/', BeneficiarioListView.as_view(), name='beneficiario_list'),
+    path('beneficiario/crear/', BeneficiarioCreateView.as_view(), name='beneficiario_create'),
+    path('beneficiario/editar/<int:pk>/', BeneficiarioUpdateView.as_view(), name='beneficiario_update'),
+    path('beneficiario/eliminar/<int:pk>/', BeneficiarioDeleteView.as_view(), name='beneficiario_delete'),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,175 +1,34 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-from django.contrib import messages
-from django.db import transaction
-
-from .models import Usuario, Transaccion, Moneda, Beneficiario
-from .forms import UsuarioForm, TransaccionForm, BeneficiarioForm, DepositoForm
-
-
-# USUARIO
-
-class UsuarioListView(ListView):
-    model = Usuario
-    template_name = 'gestion/usuario_list.html'
-    context_object_name = 'usuarios'
-
-
-class UsuarioCreateView(CreateView):
-    model = Usuario
-    template_name = 'gestion/usuario_form.html'
-    form_class = UsuarioForm
-    success_url = reverse_lazy('usuario_list')
-
-
-class UsuarioUpdateView(UpdateView):
-    model = Usuario
-    template_name = 'gestion/usuario_form.html'
-    form_class = UsuarioForm
-    success_url = reverse_lazy('usuario_list')
-
-
-class UsuarioDeleteView(DeleteView):
-    model = Usuario
-    template_name = 'gestion/usuario_confirm_delete.html'
-    success_url = reverse_lazy('usuario_list')
-
-
-# MONEDA
-
-class MonedaListView(ListView):
-    model = Moneda
-    template_name = 'gestion/moneda_list.html'
-    context_object_name = 'monedas'
-
-
-class MonedaCreateView(CreateView):
-    model = Moneda
-    template_name = 'gestion/moneda_form.html'
-    fields = ['nombre_moneda', 'simbolo']
-    success_url = reverse_lazy('moneda_list')
-
-
-class MonedaUpdateView(UpdateView):
-    model = Moneda
-    template_name = 'gestion/moneda_form.html'
-    fields = ['nombre_moneda', 'simbolo']
-    success_url = reverse_lazy('moneda_list')
-
-
-class MonedaDeleteView(DeleteView):
-    model = Moneda
-    template_name = 'gestion/moneda_confirm_delete.html'
-    success_url = reverse_lazy('moneda_list')
-
-
 # BENEFICIARIO
+
+class BeneficiarioListView(ListView):
+    model = Beneficiario
+    template_name = 'gestion/beneficiario_list.html'
+    context_object_name = 'beneficiarios'
+
+    # Muestro todos los beneficiarios
+
 
 class BeneficiarioCreateView(CreateView):
     model = Beneficiario
     form_class = BeneficiarioForm
     template_name = 'gestion/beneficiario_form.html'
-    success_url = reverse_lazy('transaccion_create')
+    success_url = reverse_lazy('beneficiario_list')
+
+    # Creo un beneficiario
 
 
-# TRANSACCIONES
+class BeneficiarioUpdateView(UpdateView):
+    model = Beneficiario
+    form_class = BeneficiarioForm
+    template_name = 'gestion/beneficiario_form.html'
+    success_url = reverse_lazy('beneficiario_list')
 
-class TransaccionListView(ListView):
-    model = Transaccion
-    template_name = 'gestion/transaccion_list.html'
-    context_object_name = 'transacciones'
-
-
-class TransaccionCreateView(CreateView):
-    model = Transaccion
-    form_class = TransaccionForm
-    template_name = 'gestion/transaccion_form.html'
-    success_url = reverse_lazy('transaccion_list')
-
-    def form_valid(self, form):
-
-        emisor = form.cleaned_data['id_usuario_emisor']
-        importe = form.cleaned_data['importe']
-
-        if emisor.saldo < importe:
-            messages.error(self.request, "Saldo insuficiente.")
-            return self.form_invalid(form)
-
-        try:
-            with transaction.atomic():
-                emisor.saldo -= importe
-                emisor.save()
-                response = super().form_valid(form)
-
-            messages.success(self.request, "Transacción realizada correctamente.")
-            return response
-
-        except Exception as e:
-            messages.error(self.request, f"Error: {str(e)}")
-            return self.form_invalid(form)
+    # Edito un beneficiario existente
 
 
-# DEPOSITO
+class BeneficiarioDeleteView(DeleteView):
+    model = Beneficiario
+    template_name = 'gestion/beneficiario_confirm_delete.html'
+    success_url = reverse_lazy('beneficiario_list')
 
-class DepositoCreateView(FormView):
-    template_name = 'gestion/deposito_form.html'
-    form_class = DepositoForm
-    success_url = reverse_lazy('usuario_list')
-
-    def form_valid(self, form):
-
-        usuario = form.cleaned_data['usuario']
-        monto = form.cleaned_data['monto']
-
-        try:
-            with transaction.atomic():
-
-                beneficiario, _ = Beneficiario.objects.get_or_create(
-                    nombre="DEPOSITO"
-                )
-
-                usuario.saldo += monto
-                usuario.save()
-
-                Transaccion.objects.create(
-                    id_usuario_emisor=usuario,
-                    id_beneficiario=beneficiario,
-                    currency_id=Moneda.objects.first(),
-                    importe=monto
-                )
-
-            messages.success(
-                self.request,
-                f"Depósito realizado correctamente. Nuevo saldo: {usuario.saldo}"
-            )
-
-            return super().form_valid(form)
-
-        except Exception as e:
-            messages.error(self.request, f"Error al realizar depósito: {str(e)}")
-            return self.form_invalid(form)
-
-
-# CARTOLA
-
-class CartolaUsuarioView(ListView):
-    model = Transaccion
-    template_name = 'gestion/cartola.html'
-    context_object_name = 'movimientos'
-
-    def get_queryset(self):
-        usuario_id = self.kwargs.get('usuario_id')
-        return Transaccion.objects.filter(
-            id_usuario_emisor_id=usuario_id
-        ).order_by('-fecha_transaccion')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        usuario_id = self.kwargs.get('usuario_id')
-        usuario = Usuario.objects.get(pk=usuario_id)
-
-        # agrego el usuario completo (incluye saldo)
-        context['usuario'] = usuario
-
-        return context
+    # Elimino un beneficiario
