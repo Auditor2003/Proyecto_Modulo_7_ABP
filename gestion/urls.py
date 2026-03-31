6eb92365d520cd10696038f140f7d0bb34fc80d1
Auditor2003 <diegomunozlasanta@gmail.com>
2026-03-30 21:22:16 -0300

    Corrige rutas moneda_update y moneda_delete en urls

--- gestion/urls.py
+++ gestion/urls.py
@@ -4,19 +4,29 @@ from .views import *
 urlpatterns = [
     path('', UsuarioListView.as_view(), name='usuario_list'),
 
-    # CRUD Usuario completo
+    # 
+    # USUARIO
+    # 
     path('crear/', UsuarioCreateView.as_view(), name='usuario_create'),
     path('editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario_update'),
     path('eliminar/<int:pk>/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 
-    # Moneda
+    # 
+    # MONEDA
+    # 
     path('moneda/', MonedaListView.as_view(), name='moneda_list'),
     path('moneda/crear/', MonedaCreateView.as_view(), name='moneda_create'),
+    path('moneda/editar/<int:pk>/', MonedaUpdateView.as_view(), name='moneda_update'),
+    path('moneda/eliminar/<int:pk>/', MonedaDeleteView.as_view(), name='moneda_delete'),
 
-    # Transacciones
+    # 
+    # TRANSACCIONES
+    # 
     path('transaccion/', TransaccionListView.as_view(), name='transaccion_list'),
     path('transaccion/crear/', TransaccionCreateView.as_view(), name='transaccion_create'),
 
-    # Beneficiario
+    # 
+    # BENEFICIARIO
+    #
     path('beneficiario/crear/', BeneficiarioCreateView.as_view(), name='beneficiario_create'),
 ]
