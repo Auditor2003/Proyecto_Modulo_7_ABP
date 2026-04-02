e5d7adb1bc7bacf93b9db190ac6282f99722e8ad
Auditor2003 <diegomunozlasanta@gmail.com>
2026-04-02 17:59:24 -0300

    Registra modelos en django admin para gestion interna

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario, Moneda, Transaccion, Beneficiario
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Moneda)
+admin.site.register(Transaccion)
+admin.site.register(Beneficiario)
